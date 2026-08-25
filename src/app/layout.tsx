0144ce62ca22dd20cd2206d34905ebdadc2e6649
Yorihito Tada <yorihitotada@hotmail.com>
2026-08-25 23:38:27 +0900

    Add src/app/layout.tsx

---
+++ src/app/layout.tsx
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: '星空マップ',
+}
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="ja">
+      <body>{children}</body>
+    </html>
+  )
+}
